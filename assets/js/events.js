3a90b7b207699b1378384ef4b4431c012075ca4e
Kamoy <soykamoy@gmail.com>
2023-02-03 07:07:29 -0500

    Add additional styling

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,19 +1,21 @@
-//Get the button
-let toTopButton = document.getElementById("toTopBtn");
+
+window.onscroll = function() {
+    navScroll();
+    showButton();
+};
+
+//Get the scroll to top button
+let toTopButton = document.getElementById("scrollTopBtn");
 
 // When the user scrolls down 745px from the top of the document, show the button
-window.onscroll = function () {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 745 || document.documentElement.scrollTop> 745) {
+function showButton() {
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700){
         toTopButton.style.display = "block";
-        // toTopButton.classList.add("show");
-    } 
-    else {
-        toTopButton.style.display = "none";
-        // toTopButton.classList.remove("show");
     }
-}
+    else {
+        toTopButton.style.display = "none";    
+    }
+} 
 
 // When the user clicks on the button, scroll to the top of the document
 toTopButton.addEventListener("click", backToTop);
@@ -27,14 +29,12 @@ function backToTop() {
 
 
 /*Change color of Nav Bar*/
-window.onscroll = function() {navScroll()};
-
 function navScroll() {
-    if (document.body.scrollTop > 657 || document.documentElement.scrollTop > 657) {
+    if (document.body.scrollTop > 980 || document.documentElement.scrollTop > 980) {
         document.getElementById("menubar").classList.add("navScroll");
     }
     else{
-        document.getElementById("menubar").classList.remove("navScroll");
+        document.getElementById("menubar").classList.remove("navScroll");        
     }
 
 }
@@ -43,9 +43,9 @@ function navScroll() {
 /*Animate and change text */
 function changeText() {
     var myAnim = document.getElementById("heroAnim");
-    myAnim.innerHTML = "the Enthusiast";
+    myAnim.innerHTML = "an Enthusiast";
     setTimeout(function () {
-        myAnim.innerHTML = "the Engineer";
+        myAnim.innerHTML = "a passionate developer";
     }, 2000);
     setTimeout(function () {
         myAnim.innerHTML = "Kamoy";
@@ -55,3 +55,38 @@ function changeText() {
 changeText();
 setInterval(changeText, 10000);
 
+/*Flip skills icons on mouse over */
+// const skillIcons = document.getElementsByClassName("skillIcons");
+// console.log(skillIcons);
+// for (const icon of skillIcons) {
+//     icon.addEventListener("mouseover", function onmouseover(){
+//         icon.classList.add("wow animate__bounceIn");
+//         icon.style.display = "#none";
+//     });
+// }
+
+// function flip() {
+//     skillIcons.classList.add("wow animate__bounceIn");
+//     skillIcons.style.display = "#none";
+// }
+
+
+// Show or hide Project Details
+let card_Details = document.getElementsByClassName("work-body");
+let cardIndex;
+
+function currentDetail(item) {
+    showDetails(cardIndex = item);
+}
+
+function showDetails(cardIndex) {
+    card_Details[cardIndex].style.display = "block";
+    document.getElementsByClassName("showMore")[cardIndex].style.display = "none";
+    document.getElementsByClassName("closeMore")[cardIndex].style.display = "block";
+}
+
+function hideDetails(cardIndex) {
+    card_Details[cardIndex].style.display = "none";
+    document.getElementsByClassName("showMore")[cardIndex].style.display = "block";
+    document.getElementsByClassName("closeMore")[cardIndex].style.display = "none";
+}
